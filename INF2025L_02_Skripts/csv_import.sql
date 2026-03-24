df30a301e502dbdf428e514c371c85ba180aaaca
Raffael Dysli <rdy158637@stud.gibb.ch>
2026-03-24 09:22:57 +0100

    first commit

---
+++ INF2025L_02_Skripts/csv_import.sql
@@ -0,0 +1,73 @@
+USE krankenhaus;
+
+LOAD DATA LOCAL INFILE 'C:abteilung.csv'
+INTO TABLE abteilung
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(id, name);
+
+LOAD DATA LOCAL INFILE 'C:\\Daten\\CSV\\zimmer.csv'
+INTO TABLE zimmer
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(id, zimmer_nr, anz_bett, abteilung_id);
+
+LOAD DATA LOCAL INFILE 'C:\\Daten\\CSV\\arzt.csv'
+INTO TABLE arzt
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(id, vorname, nachname, abteilung_id);
+
+LOAD DATA LOCAL INFILE 'C:\\Daten\\CSV\\pflegekraft.csv'
+INTO TABLE pflegekraft
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(id, vorname, nachname, abteilung_id);
+
+LOAD DATA LOCAL INFILE 'C:\\Daten\\CSV\\medikament.csv'
+INTO TABLE medikament
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(id, name, lagerbestand);
+
+LOAD DATA LOCAL INFILE 'C:\\Daten\\CSV\\patient.csv'
+INTO TABLE patient
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(id, vorname, nachname, patienten_nr);
+
+LOAD DATA LOCAL INFILE 'C:\\Daten\\CSV\\behandlung.csv'
+INTO TABLE behandlung
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(id,eintrit_datum,austrits_datum,notizen,artzt_id,patient_id,zimmer_id);
+
+LOAD DATA LOCAL INFILE 'C:\\Daten\\CSV\\behandlung_medikament.csv'
+INTO TABLE behandlung_medikament
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(behandlung_id, medikament_id);
+
+LOAD DATA LOCAL INFILE 'C:\\Daten\\CSV\\pflegekraft_behandlungen.csv'
+INTO TABLE pflegekraft_behandlung
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(pflegekraft_id, behandlung_id);
---
+++ projekt_M164/projekt_M164/doku.md
@@ -0,0 +1,15 @@
+
+```table-of-contents
+```
+# Zweck
+# Tabellen und Beziehungen
+In der datenbanck ist der haubt träger die Behandlungen sie bringt Patienten ärzte Pflegepersonal medikamente und zimmer. Die zimmer und das personal gehören jeweils zu einer abteilung.  
+# Wahl des DMS
+Wir haben Mariadb gewält da es open source schnell und 
+# Datenmodelle
+
+## Konzeptionelles Datenmodell
+![[M164_KonzeptionellesDiagramm.jpg]]
+## Logisches Datenmodell
+![[M164_LogischesDiagramm.jpg]] 
+
